f08b7382949504fca6b4c54dc28d15c8e572a5c7
bahram shamsollahi <bahram_sh@hotmail.com>
2018-12-12 19:45:44 -0800

    added entity frame work and repository

---
+++ BestRecruit/DatabaseAccess/IRepositories.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace DataRepository
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        bool Add(TEntity entity);
+        bool Add(IEnumerable<TEntity> items);
+        bool Update(TEntity entity);
+        bool Delete(TEntity entity);
+        bool Delete(IEnumerable<TEntity> entities);
+        TEntity FindBy(int id);
+        TEntity FindBy<TV>(TV id);
+        IQueryable<TEntity> All();
+        TEntity FindBy(Expression<Func<TEntity, bool>> expression);
+        IQueryable<TEntity> FilterBy(Expression<Func<TEntity, bool>> expression);
+    }
+}
---
+++ BestRecruit/DatabaseAccess/Repositories.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataRepository
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        public bool Add(TEntity entity)
+        {
+            DbSet<TEntity> d = new DbSet<TEntity>();
+
+        public bool Add(IEnumerable<TEntity> items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<TEntity> All()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(IEnumerable<TEntity> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<TEntity> FilterBy(Expression<Func<TEntity, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity FindBy(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity FindBy<TV>(TV id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity FindBy(Expression<Func<TEntity, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
